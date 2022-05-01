28e9bc729fd96b5284d2f229fa3180af11d663bf
Asiones <asionesjia@gmail.com>
2022-05-01 18:06:33 +0800

    前端第二版

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+        // screens: {
+        //     sm: '480px',
+        //     md: '768px',
+        //     lg: '976px',
+        //     xl: '1440px'
+        // },
+        extend: {
+            colors: {
+                lightOrange: '#F27457',
+                darkOrange: '#BF665E',
+                darkBlue: '#253659',
+                darkerBlue: '#2B3F66',
+                veryDarkBlue: '#1B2740',
+                darkGreen: '#03A696',
+                lightGreen: '#04BF9D',
+            }
+        },
+    },
+    plugins: [],
+}
